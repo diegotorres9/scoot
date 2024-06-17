ddefeca357ff1240b6e061860cf8d89bc790d48a
Diego Torres <diegoetorres1@gmail.com>
2024-06-17 18:55:08 -0400

    added overflow: hidden to stop scrolling when menu is open

--- scripts.js
+++ scripts.js
@@ -7,9 +7,11 @@ menuHamburger.addEventListener('click', (e) => {
     menuHamburger.style.visibility = 'hidden';
     mobileMenu.style.display = 'flex';
     closeMenu.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
 });
 closeMenu.addEventListener('click', (e) => {
     menuHamburger.style.visibility = 'visible';
     mobileMenu.style.display = 'none';
     closeMenu.style.display = 'none';
+    document.body.style.overflow = 'visible';
 });
